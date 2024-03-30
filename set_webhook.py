2aa29b4b445de557be753c06fdec2ed9e09bd4f4
M. Ajun Setiawan <71912420+setiaone-tech@users.noreply.github.com>
2024-03-30 11:40:28 +0700

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,7 +1,9 @@
 import requests
 
-TOKEN = "5553337166:AAF1y4oHltscYMjSyr6UlQ5BpxjmxNpza9o"  # Ganti dengan token bot Anda
-WEBHOOK_URL = "https://calamity94.masuk.id/webhook"
+#Token API Telegram BOT
+TOKEN = ""
+#Your Website for Webhhook
+WEBHOOK_URL = "{url}/webhook"
 
 requests.get(f"https://api.telegram.org/bot{TOKEN}/deleteWebhook")
 requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}")
